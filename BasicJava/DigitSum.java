fe32e0c303550ccf28ac3e481912f7c0a94c29a2
Sumanth Reddy M <145849062+sumanth6305@users.noreply.github.com>
2025-06-08 14:45:30 +0530

    Update DigitSum.java

--- BasicJava/DigitSum.java
+++ BasicJava/DigitSum.java
@@ -3,7 +3,7 @@ import java.util.*;
 class DigitSum{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number to print fibonocci series : ");
+		System.out.println("Enter number : ");
 		int n = sc.nextInt();
 		int temp  = n;
 		int digit,sum=0;
@@ -12,7 +12,7 @@ class DigitSum{
 			n=n/10;
 			sum= sum+digit;
 		}
-		System.out.println(sum);
+		System.out.println("Digit sum = "+sum);
 	}
 }
 
