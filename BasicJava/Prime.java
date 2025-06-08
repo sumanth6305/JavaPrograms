3f399602e9a4c2c891b279143d4cd796810b1809
Sumanth Reddy M <145849062+sumanth6305@users.noreply.github.com>
2025-06-08 14:46:10 +0530

    Update Prime.java

--- BasicJava/Prime.java
+++ BasicJava/Prime.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Prime{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number to print fibonocci series : ");
+		System.out.println("Enter number : ");
 		int n = sc.nextInt();
 		for(int i=2;i<n;i++){
 			if(n%i==0){
