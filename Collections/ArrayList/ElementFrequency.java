4be9aab5efd18f1223c6ace29168991ca90bcc16
Sumanth Reddy M <145849062+sumanth6305@users.noreply.github.com>
2025-08-07 16:14:54 +0530

    Create ElementFrequency.java

---
+++ Collections/ArrayList/ElementFrequency.java
@@ -0,0 +1,13 @@
+//Write a program to find the frequency of each element in an ArrayList.
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main
+{
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9));
+		Iterator<Integer> iterator = list.iterator();
+		
+		
+	}
+}
